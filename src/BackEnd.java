778193e039e2b90ba9a7d411c4518af5842c78d7
samfr2 <samcfrizzell@yahoo.com>
2015-04-06 12:47:14 -0500

    classes

---
+++ src/BackEnd.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+
+public class BackEnd {
+
+	public List<String> getMessages(String username){
+		
+		return null;
+	}
+	
+	public void addMessage(String username,String msg){
+		
+	}
+	
+	public List<String> getUsers(){
+		return null;
+	}
+	
+	
+}
