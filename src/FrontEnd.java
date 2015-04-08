c9dd9840815bb29848f365595d41e11dfd32427d
andrewhall17 <andrewcooperhall@gmail.com>
2015-04-08 12:54:04 -0500

    Made Front End Better Sort of

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -36,71 +36,80 @@ public class FrontEnd {
 		vBox.add(subtitle2Box);
 		vBox.add(finduserBox);
 		vBox.add(outputBox);
-		
+		frame.getContentPane().setBackground(Color.BLACK);
 		//titleBox
 		JLabel leftStar = new JLabel();
 		JLabel title = new JLabel("Messaging");
 		JLabel rightStar = new JLabel();
 		
 		title.setFont(new Font("Comic Sans MS", Font.BOLD, 28));
-		
+		title.setForeground(Color.WHITE);
 		Icon star = new ImageIcon("electricity.gif");
 		leftStar.setIcon(star);
 		rightStar.setIcon(star);
+		leftStar.setPreferredSize(new Dimension(175, 75));
+		leftStar.setMaximumSize(new Dimension(175,75));
+		leftStar.setMinimumSize(new Dimension(175,75));
+		rightStar.setPreferredSize(new Dimension(175, 75));
+		rightStar.setMaximumSize(new Dimension(175,75));
+		rightStar.setMinimumSize(new Dimension(175,75));
 		titleBox.add(leftStar);
 		titleBox.add(title);
 		titleBox.add(rightStar);
 		
 		//subtitleBox
 		JLabel subtitle = new JLabel("Add Message");
-		subtitle.setFont(new Font("Comic Sans MS", Font.ITALIC, 18));
+		subtitle.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+		subtitle.setForeground(Color.WHITE);
 		subtitleBox.add(subtitle);
 		
 		//addmessuserBox
 		JLabel user = new JLabel("User:");
 		user.setFont(new Font("Comic Sans MS",Font.PLAIN, 12));
-		
+		user.setForeground(Color.WHITE);
 		String[] users = new String[0];
 		users = myBackEnd.getUsers().toArray(users);
 		JComboBox<String> comboBox = new JComboBox<String>(users);
+		addmessuserBox.add(Box.createHorizontalStrut(100));
 		addmessuserBox.add(user);
 		addmessuserBox.add(comboBox);
-		
+		addmessuserBox.add(Box.createHorizontalStrut(100));
 		//addmessBox
 		JLabel message = new JLabel("Message:");
 		message.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+		message.setForeground(Color.WHITE);
 		JTextField messagefield = new JTextField();
+		addmessBox.add(Box.createHorizontalStrut(81));
 		addmessBox.add(message);
 		addmessBox.add(messagefield);
-		
+		addmessBox.add(Box.createHorizontalStrut(100));
 		//okBox
 		JButton ok = new JButton("OK");
 		okBox.add(ok);
 		
 		//lineBox
 		JPanel line = new JPanel();
-		line.setBackground(Color.BLACK);
-		line.setMaximumSize(new Dimension(400, 5));
-		
-		
-		lineBox.add(Box.createHorizontalStrut(100));
+		line.setBackground(Color.WHITE);
+		line.setMaximumSize(new Dimension(600, 5));
+
 		lineBox.add(line);
-		lineBox.add(Box.createHorizontalStrut(100));
 		
 		//subtitle2Box
 		JLabel subtitle2 = new JLabel("Get Message");
-		subtitle.setFont(new Font("Comic Sans MS", Font.ITALIC, 18));
-		subtitleBox.add(subtitle2);
+		subtitle2.setFont(new Font("Comic Sans MS", Font.ITALIC, 18));
+		subtitle2.setForeground(Color.WHITE);
+		subtitle2Box.add(subtitle2);
 		
 		//finduserBox
 		JLabel user2 = new JLabel("User:");
-		user.setFont(new Font("Comic Sans MS",Font.PLAIN, 12));
+		user2.setFont(new Font("Comic Sans MS",Font.PLAIN, 12));
+		user2.setForeground(Color.WHITE);
 		users = myBackEnd.getUsers().toArray(users);
 		JComboBox<String> comboBox2 = new JComboBox<String>(users);
 		JButton ok2 = new JButton("OK");
-		subtitle2Box.add(user2);
-		subtitle2Box.add(comboBox2);
-		subtitle2Box.add(ok2);
+		finduserBox.add(user2);
+		finduserBox.add(comboBox2);
+		finduserBox.add(ok2);
 		
 		//outputBox
 		final JTextPane defPane = new JTextPane();
